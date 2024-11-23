8b6dea21b52bdf348812084d2e98218808238e56
memaar92 <marius.messerschmied@gmail.com>
2024-11-23 21:23:40 +0100

    Initial Commit

---
+++ SwiftyCompanion/SwiftyCompanionApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftyCompanionApp.swift
+//  SwiftyCompanion
+//
+//  Created by Marius Messerschmied on 23.11.24.
+//
+
+import SwiftUI
+
+@main
+struct SwiftyCompanionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ SwiftyCompanionTests/SwiftyCompanionTests.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftyCompanionTests.swift
+//  SwiftyCompanionTests
+//
+//  Created by Marius Messerschmied on 23.11.24.
+//
+
+import Testing
+@testable import SwiftyCompanion
+
+struct SwiftyCompanionTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
